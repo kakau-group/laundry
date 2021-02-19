4545da609d73b10fefe466f63ec465d7a28a8683
kakau <fadjarsetiawans@gmail.com>
2021-02-19 14:33:42 +0700

    fixed tombol chat

--- pages/atas.php
+++ pages/atas.php
@@ -1,101 +1,113 @@
 <!DOCTYPE html>
 <html lang="en-US" dir="ltr">
 
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-    <!-- ===============================================-->
-    <!--    Document Title-->
-    <!-- ===============================================-->
-    <title>Kakau Laundry</title>
-    
-    <!-- ===============================================-->
-    <!--    Favicons-->
-    <!-- ===============================================-->
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
-    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicons/favicon.ico">
-    <link rel="manifest" href="assets/img/favicons/manifest.json">
-    <meta name="msapplication-TileImage" content="assets/img/favicons/mstile-150x150.png">
-    <meta name="theme-color" content="#ffffff">
+  <!-- ===============================================-->
+  <!--    Document Title-->
+  <!-- ===============================================-->
+  <title>Kakau Laundry</title>
 
-    <!-- ===============================================-->
-    <!--    Stylesheets-->
-    <!-- ===============================================-->
-    <link href="https://fonts.googleapis.com/css?family=Poppins:400,300&amp;display=swap" rel="stylesheet">
-    <link href="assets/css/theme.css" rel="stylesheet">
+  <!-- ===============================================-->
+  <!--    Favicons-->
+  <!-- ===============================================-->
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicons/favicon-16x16.png">
+  <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicons/favicon.ico">
+  <link rel="manifest" href="assets/img/favicons/manifest.json">
+  <meta name="msapplication-TileImage" content="assets/img/favicons/mstile-150x150.png">
+  <meta name="theme-color" content="#ffffff">
 
-
-    <!-- My Css -->
-    <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/8a5c46fe5e.js" crossorigin="anonymous"></script>
+  <!-- ===============================================-->
+  <!--    Stylesheets-->
+  <!-- ===============================================-->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,300&amp;display=swap" rel="stylesheet">
+  <link href="assets/css/theme.css" rel="stylesheet">
 
 
-    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
-    
-    
-
-  </head>
-  <body>
+  <!-- My Css -->
+  <link rel="stylesheet" href="style.css">
+  <script src="https://kit.fontawesome.com/8a5c46fe5e.js" crossorigin="anonymous"></script>
 
 
-   <div style="position:fixed;left:20px;bottom:20px;">
-      <a href="https://api.whatsapp.com/send?phone=+6281223606493&text=Halo">
-      <a class="btn btn-success  ml-auto hover-top-shadow" href="https://api.whatsapp.com/send?phone=+6281223606493&text=Halo"><h7> CHAT SEKARANG</h7></a>
-      </a>
-    </div>
+  <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
 
-    <!-- ===============================================-->
-    <!--    Main Content-->
-    <!-- ===============================================-->
-    <main class="main">
-      <nav class="navbar navbar-expand-lg navbar-light bg-white py-4 pl-0">
-        <div class="container"><a class="navbar-brand text-primary font-weight-bold" href="index.php"> Kakau Laundry </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item text-muted"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
+  <style>
+    .wabutton {
+      width: 170px;
+      height: 70px;
+      position: fixed;
+      bottom: 20px;
+      right: 20px;
+      z-index: 100;
+    }
+  </style>
 
-              <li class="nav-item dropdown"><a class="nav-link" id="navbarDropdown" href="#" role="button" data-toggle="dropdown" aria-expanded="false">Profil</a>
-                <ul class="dropdown-menu navbar-dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item text-secondary p-3 border-bottom" href="mengapa-kakau-laundry.php">Mengapa Kakau Laundry</a></li>
-                  <li><a class="dropdown-item text-secondary p-3" href="laundry-kiloan.php">Laundry Kiloan Cepat dan Express </a></li>
-                </ul>
-              </li>
+</head>
 
-              <li class="nav-item dropdown"><a class="nav-link" id="navbarDropdown" href="#" role="button" data-toggle="dropdown" aria-expanded="false">Investasi</a>
-                <ul class="dropdown-menu navbar-dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item text-secondary p-3 border-bottom" href="tahapan-menjadi-franchisee.php">Tahapan Menjadi Mitra</a></li>
-                  <li><a class="dropdown-item text-secondary p-3 border-bottom" href="pilihan-paket-investasi.php">Pilih Paket Investasi</a></li>
-                  <li><a class="dropdown-item text-secondary p-3" href="dukungan-sistem-dan-manajemen.php">Dukungan Sistem Manajemen </a></li>
-                </ul>
-              </li>
+<body>
 
-              <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
-       
-          
-                <li>
-                <a class="btn btn-primary ml-auto hover-top-shadow" href="daftar-member.php">Daftar Sebagai Member</a>
-                </li>
-            
-                <div>
-                  &nbsp;
-                </div>
 
-                <li>
-                <a class="btn btn-primary  ml-auto hover-top-shadow" href="daftar-investor.php">Daftar Sebagai Investor</a>
-                </li>
 
-            </ul>
-          </div>
+
+  <a href="https://api.whatsapp.com/send?phone=+6281223606493&text=Permisi, Saya memiliki pertanyaan seputar Kakau Laundry">
+    <img src="https://rizbamart.com/blog/wp-content/uploads/2019/10/HUBUNGI-WA-greenangelica.png" class="wabutton" alt="WhatsApp-Button">
+  </a>
+
+
+
+  <!-- ===============================================-->
+  <!--    Main Content-->
+  <!-- ===============================================-->
+  <main class="main">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white py-4 pl-0">
+      <div class="container"><a class="navbar-brand text-primary font-weight-bold" href="index.php"> Kakau Laundry </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item text-muted"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
+
+            <li class="nav-item dropdown"><a class="nav-link" id="navbarDropdown" href="#" role="button" data-toggle="dropdown" aria-expanded="false">Profil</a>
+              <ul class="dropdown-menu navbar-dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item text-secondary p-3 border-bottom" href="mengapa-kakau-laundry.php">Mengapa Kakau Laundry</a></li>
+                <li><a class="dropdown-item text-secondary p-3" href="laundry-kiloan.php">Laundry Kiloan Cepat dan Express </a></li>
+              </ul>
+            </li>
+
+            <li class="nav-item dropdown"><a class="nav-link" id="navbarDropdown" href="#" role="button" data-toggle="dropdown" aria-expanded="false">Investasi</a>
+              <ul class="dropdown-menu navbar-dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item text-secondary p-3 border-bottom" href="tahapan-menjadi-franchisee.php">Tahapan Menjadi Mitra</a></li>
+                <li><a class="dropdown-item text-secondary p-3 border-bottom" href="pilihan-paket-investasi.php">Pilih Paket Investasi</a></li>
+                <li><a class="dropdown-item text-secondary p-3" href="dukungan-sistem-dan-manajemen.php">Dukungan Sistem Manajemen </a></li>
+              </ul>
+            </li>
+
+            <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+
+
+            <li>
+              <a class="btn btn-primary ml-auto hover-top-shadow" href="daftar-member.php">Daftar Sebagai Member</a>
+            </li>
+
+            <div>
+              &nbsp;
+            </div>
+
+            <li>
+              <a class="btn btn-primary  ml-auto hover-top-shadow" href="daftar-investor.php">Daftar Sebagai Investor</a>
+            </li>
+
+          </ul>
         </div>
-      </nav>
+      </div>
+    </nav>
 
 
-      <!-- ============================================-->
-            <!-- ============================================-->
-      <!-- <section> begin ============================-->
+    <!-- ============================================-->
+    <!-- ============================================-->
+    <!-- <section> begin ============================-->
