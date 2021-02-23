d15fad0102284febc165d7925200638a3dfd823f
fadjarsetiawan <fadjarsetiawans@gmail.com>
2021-02-23 01:35:56 -0800

    ganti jumbotron

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include("atas.php");
         <div id="carousel-example" class="carousel slide" data-ride="carousel">
           <div class="carousel-inner">
             <div class="carousel-item active">
-              <img src="img/kakau-laundry-slide.png" style="max-height: 500px;" class="d-block w-100" alt="First slide">
+              <img src="img/kakau-laundry-slide1.png" style="max-height: 500px;" class="d-block w-100" alt="First slide">
               <div class="carousel-caption">
                 <div class="description wow fadeInUp">
                   </p>
