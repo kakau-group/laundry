5fde1d25e0f681a76030fe96fd42bf898e01f5ae
fadjar setiawan <fadjarsetiawans@gmail.com>
2021-02-25 19:02:45 +0700

    tambah maps di bawah

--- bawah.php
+++ bawah.php
@@ -11,7 +11,7 @@
     <footer class="site-footer bg-dark" role="contentinfo">
       <div class="container">
         <div class="row">
-          <div class="col-md-3">
+          <div class="col-md-2">
             <p>
             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
             &copy;<script>document.write(new Date().getFullYear());</script> <a >Kakau (fdrs)</a>
@@ -22,14 +22,24 @@
             <ul class="list-unstyled footer-link">
               <li><span class="mr-3 d-block">Alamat:</span><span class="text-white">Jl. Raya Tengah Tani No.59, Dawuan, Kec. Tengah Tani, Kab. Cirebon, Jawa Barat, Indonesia</span></li>
               <li><span class="mr-3 d-block">Telfon:</span><span class="text-white">+62 823-2131-7255</span></li>
-              <li><span class="mr-3 d-block">E-mail:</span><span class="text-white">info@yourdomain.com</span></li>
+              <li><span class="mr-3 d-block">E-mail:</span><span class="text-white">admin@kakau.id</span></li>
             </ul>
           </div>
-          <div class="col-md-3">
+          <div class="col-md-2">
             <h3 class="text-white">Social</h3>
             <ul class="list-unstyled footer-link d-flex footer-social">
               <li><a href="https://www.facebook.com/KakauLaundry" class="p-2"><span class="fa fa-facebook"></span></a></li>
               <li><a href="https://www.instagram.com/kakau_laundry" class="p-2"><span class="fa fa-instagram"></span></a></li>
+            </ul>
+          </div>
+          <div class="col-md-3">
+            <h3 class="text-white">Maps</h3>
+            <ul class="list-unstyled footer-link d-flex footer-social">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.501138706389!2d108.52008591431506!3d-6.708526167453035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6ee14f3e1117f3%3A0xa4258a10676f5a8a!2sKakau%20Laundry!5e0!3m2!1sid!2sid!4v1612935840302!5m2!1sid!2sid" width="500" height="500" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
+                </iframe>
+                </iframe>
+              </div>
             </ul>
           </div>
         </div>
